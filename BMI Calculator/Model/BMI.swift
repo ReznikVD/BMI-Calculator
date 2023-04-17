fa28c4247c4718bba86c91f8691a170ee033c5a3
Vladislav Reznik <vladek_1999@mail.ru>
2023-04-17 18:42:39 +0300

    add logic and UI

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Владислав Резник on 17.04.2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
